d83425a2420038a7ee31e318676321953b33b1e4
Florian Eggenhofer <egg@tbi.univie.ac.at>
2015-02-11 18:56:27 +0100

    Added Links to iteration alignment and model files to result page

--- Handler/Result.hs
+++ Handler/Result.hs
@@ -25,6 +25,8 @@ getResultR = do
     approot  <- fmap extraApproot getExtra
     outputPath <- fmap extraTempdir getExtra
     let temporaryDirectoryPath = DT.unpack (outputPath) ++ sessionId ++ "/"
+    let tempDirectoryRootURL = "http://nibiru.tbi.univie.ac.at/rnalien_tmp/rnalien/"
+    let tempDirectoryURL = "http://nibiru.tbi.univie.ac.at/rnalien_tmp/rnalien/" ++ sessionId ++ "/"
     --check if tempdir exists otherwise short circuit
     tempDirPresent <- liftIO (doesDirectoryExist temporaryDirectoryPath)
     --checkSessionId tempDirPresent             
@@ -32,9 +34,8 @@ getResultR = do
     done <- liftIO (doesFileExist (temporaryDirectoryPath ++ "done"))
     let unfinished = not done        
     alienLog <- liftIO (readFile (temporaryDirectoryPath ++ "Log"))
-    let iterationLogPaths = map (\x -> temporaryDirectoryPath ++ (show x) ++ ".log") [0,1,2,3,4,5,6,7,8,9,10]
-    existentIterationLogs <- liftIO (filterM doesFileExist iterationLogPaths)
-    iterationLogs <- liftIO (mapM retrieveIterationLog existentIterationLogs)
+    existentIterationLogs <- liftIO (filterM (\x -> doesFileExist (temporaryDirectoryPath ++ (show x) ++ ".log")) [0,1,2,3,4,5,6,7,8,9,10])
+    iterationLogs <- liftIO (mapM (retrieveIterationLog temporaryDirectoryPath tempDirectoryURL) existentIterationLogs)
     
     let resultInsert = DT.pack (concat iterationLogs)
     defaultLayout $ do
@@ -42,10 +43,12 @@ getResultR = do
         setTitle "Welcome To RNAlien!"
         $(widgetFile "result")
 
-retrieveIterationLog :: String -> IO String
-retrieveIterationLog filePath = do
-      iterationLog <- readFile filePath
+retrieveIterationLog :: String -> String -> Int -> IO String
+retrieveIterationLog temporaryDirectoryPath tempDirectoryURL counter = do
+      let logPath = temporaryDirectoryPath ++ (show counter) ++ ".log"
+      iterationLog <- readFile logPath
+      let alnlink = "<a href=\"" ++ tempDirectoryURL ++ "/" ++ show counter ++ "/" ++ "model.stockholm" ++ "\">stockholm-format</a>" 
+      let cmlink = "<a href=\"" ++ tempDirectoryURL ++ "/" ++ show counter ++ "/" ++ "model.cm" ++ "\">covariance-model</a>" 
       let logfields = splitOn "," iterationLog
-      let iterationLine = "<tr><td>" ++ logfields !! 0 ++ "<td>" ++ logfields !! 1 ++ "<td>" ++ logfields !! 2 ++ "<td>" ++ logfields !! 3 ++"\n"
+      let iterationLine = "<tr><td>" ++ logfields !! 0 ++ "<td>" ++ logfields !! 1 ++ "<td>" ++ logfields !! 2 ++ "<td>" ++ logfields !! 3 ++ "<td>" ++ alnlink ++ "<td>" ++ cmlink ++"\n"
       return iterationLine
-
