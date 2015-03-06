c27758675546c0c9ec9838072279d85633cefbce
Florian Eggenhofer <florian.eggenhofer@univie.ac.at>
2015-03-06 10:19:35 +0100

    Added handlers for tool and help pages and auxiliary files

--- Application.hs
+++ Application.hs
@@ -23,6 +23,9 @@ import Yesod.Core.Types (loggerSet, Logger (Logger))
 -- Don't forget to add new modules to your cabal file!
 import Handler.Home
 import Handler.Result
+import Handler.Tool
+import Handler.Help
+    
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
 -- comments there for more details.
