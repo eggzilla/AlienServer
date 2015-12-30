78ddb46f9062b6af742d022a524b7b038034177d
Florian Eggenhofer <egg@tbi.univie.ac.at>
2015-12-30 14:08:34 +0100

    Added organism autocomplete suggestions handler internals

--- Handler/Taxonomy.hs
+++ Handler/Taxonomy.hs
@@ -4,10 +4,12 @@ module Handler.Taxonomy where
 
 import Import
 import qualified Data.Text as DT
+import qualified Data.Text.IO as DI
 import Yesod.Form.Bootstrap3
     ( BootstrapFormLayout (..), renderBootstrap3, withSmallInput )
 import Yesod.Form.Jquery
 import qualified Data.Vector as V
+import Data.Maybe
 
 getTaxonomyR :: Handler Value
 getTaxonomyR = do
@@ -16,7 +18,12 @@ getTaxonomyR = do
         case formResult of
             FormSuccess (_,inputorganism) -> return inputorganism
             _ -> return (Just $ DT.pack "")
-    return $ Array (V.fromList [String "abc",String "def"]) 
+    let organism = fromJust inputorganism
+    organismString <- liftIO (DI.readFile "/mnt/storage/data/rnalien/organismslist")
+    let organisms = V.fromList $ DT.lines $ organismString
+    let hits = V.take 10 $ V.filter (DT.isInfixOf organism) organisms
+    return $ Array (V.map String hits)
+
 
 inputForm :: Form (FileInfo, Maybe Text)
 inputForm = renderBootstrap3 BootstrapBasicForm $ (,)
